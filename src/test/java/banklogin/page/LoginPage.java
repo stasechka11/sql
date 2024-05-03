799147389e41b04c5bddef235eef24af9dccb7e2
Anastasiia Fokina <stasechka11@yandex.ru>
2024-05-03 10:49:19 +0300

    added LoginPage

---
+++ src/test/java/banklogin/page/LoginPage.java
@@ -0,0 +1,20 @@
+package banklogin.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+    private final SelenideElement loginField = $("[data-test-id=login] input");
+    private final SelenideElement passwordField = $("[data-test-id=password] input");
+    private final SelenideElement loginButton = $("[data-test-id=action-login]");
+    private final SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification_content");
+
+    public void verifyErrorNotification(String expectedText) {
+        errorNotification.shouldHave(Condition.exactText(expectedText)).shouldBe(visible);
+    }
+
+
+}
